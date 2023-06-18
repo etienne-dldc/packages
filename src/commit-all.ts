128a8fe70c712a1f84e7da48dc37bdaadaebf89b
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2023-06-19 01:05:57 +0200

    Fix

--- src/commit-all.ts
+++ src/commit-all.ts
@@ -1,14 +1,14 @@
 import { isCancel } from '@clack/core';
+import { input } from '@inquirer/prompts';
 import pc from 'picocolors';
 import { $ } from 'zx';
 import { IPackage, packages } from './packages';
-import { text } from './prompts/text';
 import { pkgUtils } from './utils/pkgUtils';
 
 main().catch(console.error);
 
 async function main() {
-  const message = await text({ message: 'Commit message' });
+  const message = await input({ message: 'Commit message' });
   if (isCancel(message)) {
     return;
   }
